f98a0acb59abb242c977c56c7acc098c461b3e63
Fluffy-Bean <michal-gdula@protonmail.com>
2024-12-07 22:39:53 +0000

    forgor to commit...

---
+++ src/lib/components/lists/ListLabel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { twMerge as tw } from "tailwind-merge";
+
+    interface Props {
+        text: string,
+        type: "headline" | "supporting",
+        class?: string,
+    }
+
+    let { text, type, class: c = "" }: Props = $props();
+</script>
+
+{#if type === "headline"}
+    <p class={tw("text-m3-on-surface text-m3-size-body-large", c)}>{ text }</p>
+{:else if type === "supporting"}
+    <p class={tw("text-m3-size-body-medium text-m3-on-surface-variant", c)}>{ text }</p>
+{/if}
